2769522e90017bdee560d3119098a891c161a3f0
Mirelle26 <160304972+Mirelle26@users.noreply.github.com>
2024-03-09 11:55:06 +0800

    Update song2.php

--- song2.php
+++ song2.php
@@ -114,7 +114,7 @@
         }
 
         .play-element:hover {
-            background-color: #ffff; /* Change background color on hover */
+            background-color: #ffff;
         }
 		footer {
             background-color: #800000;
@@ -232,7 +232,7 @@
             echo "<p>Doro[D]thea[A]</p><br><br>";
 
 
-            // Greeting echo
+           
             echo "<p style='font-size: 24px; font-weight: bold;'>Free Lyrics</p>";
         ?>
                 <footer>
